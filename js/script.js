4f2d612738e1e8e1e68cd134517c02d91d8c5e64
Jansi Vimala Mary John Bosco <jansivimalamary@gmail.com>
2025-08-12 16:46:43 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
